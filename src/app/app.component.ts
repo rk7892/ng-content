d1a702f47aa30d5e95f5bb692af3d9faa8da84af
rk7892 <ranjithraina7952@gmail.com>
2024-01-27 22:50:01 +0530

    code development

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,12 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { NgContentComponent } from './ng-content/ng-content.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, CommonModule, NgContentComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
--- src/app/ng-content/ng-content.component.ts
+++ src/app/ng-content/ng-content.component.ts
@@ -1,9 +1,10 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-ng-content',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './ng-content.component.html',
   styleUrl: './ng-content.component.css'
 })
